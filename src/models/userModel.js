7231a05340656365dc45cfd903b328465024f732
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-04-01 23:00:20 +0530

    feat: adds user model

---
+++ src/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true 
+    },
+    email: {
+        type: String,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true 
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        required: true 
+    },
+    role: {
+        type: String,
+        enum: ['viewer','analyst','admin'],
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model(userSchema, 'user')
