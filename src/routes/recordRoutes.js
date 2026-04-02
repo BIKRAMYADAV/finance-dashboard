1e0ef31d063a6b77b7e836092c3d4d24ef7eb6b1
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-04-02 20:05:54 +0530

    feat: updates recordRoutes to include middlewares

--- src/routes/recordRoutes.js
+++ src/routes/recordRoutes.js
@@ -1,5 +1,11 @@
 const recordController = require('../controllers/recordController')
+const {authMiddleware} = require('../middlewares/authMiddleware')
+const {allowRoles} = require('../middlewares/roleMiddleware')
+
 
 module.exports = (app) => {
-    app.post('/create', recordController.createRecord)
+    app.post('/records',authMiddleware, allowRoles('admin'), recordController.createRecord)
+    app.get('/records',authMiddleware, allowRoles('admin', 'viewer', 'analyst'), recordController.getRecord)
+    app.patch('/records/:id',authMiddleware,allowRoles('admin'), recordController.updateRecord)
+    app.delete('/records/:id',authMiddleware, allowRoles('admin'), recordController.deleteRecord)
 }
